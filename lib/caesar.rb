9bdaee3ee2e39fa254f0485ed953bbbc152d4e5f
Jenny Feng <jennyfeng.ga.ling1999@gmail.com>
2016-12-16 17:09:50 +0000

    add negative offset

--- lib/caesar.rb
+++ lib/caesar.rb
@@ -6,6 +6,20 @@ $uppercase = [*?A..?Z] #creates an array of uppercase letters
 # puts "Type a string"
 # string = gets.chomp
 
+def decode(string, neg_offset)
+  string.split("").collect do |characters|
+      if $lowercase.include?(characters)
+         index_value = $lowercase.index(characters) - neg_offset
+         new_string = $lowercase[index_value % 26]
+      elsif $uppercase.include?(characters)
+         index_value = $uppercase.index(characters) - neg_offset
+         new_string = $uppercase[index_value % 26]
+      else
+        characters
+      end
+    end.join("")
+end
+
 def caeser_cipher(string, offset)
     string.split("").collect do |characters|
       if $lowercase.include?(characters)
@@ -15,9 +29,10 @@ def caeser_cipher(string, offset)
          index_value = $uppercase.index(characters) + offset
          new_string = $uppercase[index_value % 26]
       else
-        characters #prints the rest of the characters that are not in the array of alphabets
+        characters
       end
     end.join("")
 end
 
-caeser_cipher("string", 2)
+# caeser_cipher("string", 2)
+decode("Jenny", 2)
